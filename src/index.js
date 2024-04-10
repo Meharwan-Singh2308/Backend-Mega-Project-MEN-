4d05ab67eb41d5e471c3a54007d02123a9585832
Meharwan-Singh2308 <meharwangamers@gmail.com>
2024-04-10 19:43:27 +0530

    Added User and Video Model and JWT and Bcrypt Libraries :: Lecture 10 Completed :)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 // require('dotenv').config({path: './env'})
 
-import connectDB from './db/index.js'
-import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+import dotenv from 'dotenv';
+import { app } from './app.js';
 
-dotenv.config({path:'./env'})
+dotenv.config({ path: './env' })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on PORT :: ${process.env.PORT}`);
+        })
+    })
+
+    .catch((error) => {
+        console.log(`MongoDB Connection Failed :: ${error}`);
+    })
 
 
+
+    app.on('error',(err) => {
+        console.log(err);
+        throw error;
+    })
 
 
 
---
+++ src/models/user.model.js
@@ -0,0 +1,110 @@
+import { Mongoose, Schema, model } from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        trim: true,
+        index: true,
+        lowercase: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        required: true
+    },
+    fullname: {
+        type: String,
+        trim: true,
+        index: true,
+        required: true,
+    },
+    avatar: {
+        type: String, // Cloudinary
+        required: true
+    },
+    coverImage: {
+        type: String, // Cloudinary
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, 'Password is Required']
+    },
+    refreshToken: {
+        type: String
+    },
+}, {
+    timestamps: true
+}
+)
+
+
+
+
+userSchema.pre("save", async function (next) {
+
+    if (!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export const User = model("User", userSchema)
